68d0b566e3d52909a5f847b255a655a0d7ce58e5
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-26 18:11:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require 'pry'
+def dictionary
+  {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end 
+
+def word_substituter(tweet)
+array = tweet.split(" ")
+array.collect do |word|
+  dictionary.collect do |key, value|
+    if word == key 
+      word.replace(value)
+    end 
+  end 
+end 
+array.join(" ") 
+end 
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end 
+end 
