35a0fe9fe94c67921370d7b1e5c1f8ebe4b43d32
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-26 18:49:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length < 140
     word_substituter(tweet)
   else
-    word_substituter(tweet)[0..139]
+    word_substituter(tweet)[0..139] + "..."
   end 
 end 
 
